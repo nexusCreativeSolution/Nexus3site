67864c608ed20e50588d5b2492447a78f8985585
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-06-16 20:14:20 +0530

    text feature in services section

--- src/sections/service-section.js
+++ src/sections/service-section.js
@@ -36,11 +36,34 @@ const data = {
 };
 
 export default function ServiceSection() {
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    setVideoOpen(true);
+  }
   return (
     <section sx={{variant : 'section.services'}}>
       <Container sx={styles.containerBox}>
         <Box sx={styles.thumbnail}>
           <Image src={ServiceThumb} alt={"Thumbnail"}/>
+          <Button
+          sx={styles.videoBtn}
+          onClick={handleClick}
+          aria-lable="Pay Button"
+          >
+            <span>
+              <IoIosPlay />
+            </span>
+            
+
+          </Button>
+          <Box sx={styles.shapeBox}>  
+          <Image src={shapePattern} alt="shape" />
+
+          </Box>
+        </Box>
+        <Box sx={styles.contentBox}>
+          <TextFeature subTitle={data.subTitle} title={data.title}/>
         </Box>
 
       </Container>
