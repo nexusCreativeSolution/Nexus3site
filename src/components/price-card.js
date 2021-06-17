a5c502ef843d772f2220f2e5f31cfb26a6b27766
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-06-17 14:37:14 +0530

    added the list items to pricing sections

--- src/components/price-card.js
+++ src/components/price-card.js
@@ -14,7 +14,22 @@ export default function PriceCard({
   },
 }) {
   return (
-    <h1>PriceCard</h1>
+    <Card
+       className={header ? 'package__card active' : 'package__card'}
+       sx={styles.pricingBox}
+       >
+       {header && <Text sx={styles.header}>{header}</Text>}
+       <Box>
+         <Box className="package__header" sx={styles.pricingHeader}>
+           <Heading className="package__name" variant="title">
+             {name}
+           </Heading>
+           <Text as="p" >{description}</Text>
+
+         </Box>
+         <List item={points} childStyle ={styles.listItem} />
+       </Box>
+       </Card>
   );
 }
 
