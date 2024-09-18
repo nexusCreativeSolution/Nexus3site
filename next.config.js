6842d94e03425afdab2174103adeb7f924b61722
tiger-dev105 <ar5051380@gmail.com>
2024-09-17 20:24:46 -0400

    Fix some bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+// module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+
+module.exports = withPlugins([optimizedImages], { 
+    experimental: {
+        target: 'serverless',
+        crypto: { encrypt: false },
+        cryptography: 'aes-256-cbc',
+      },
+      security: {
+        enableSourceMap: true,
+        ssl: {
+          enabled: false,
+        },
+      },
+});
