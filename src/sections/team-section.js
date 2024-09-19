b6a59f193aafebfaa48f5a6257d3048dcab76101
NEXUS <publicbot34@gmail.com>
2024-09-18 21:10:22 -0500

    Update team-section.js

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -1,176 +1,102 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { Container, Grid } from 'theme-ui';
-import SectionHeader from 'components/section-header';
-import TeamCard from 'components/team-card';
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
-
-import Member1 from 'assets/team/member-1.png';
-import Member2 from 'assets/team/member-2.png';
-import Member3 from 'assets/team/member-3.png';
-import Member4 from 'assets/team/member-4.png';
-import Member5 from 'assets/team/member-5.png';
-import Member6 from 'assets/team/member-6.png';
+import React from 'react';
+import { Container, Grid, Box, Image, Heading, Text, Link } from 'theme-ui';
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaFacebookF, FaDiscord } from 'react-icons/fa';
 
 const data = [
   {
     id: 1,
-    imgSrc: Member1,
-    altText: 'Saimon Harmer',
-    title: 'Saimon Harmer',
-    designation: 'CEO and Founder',
+    imgSrc: 'https://telegra.ph/file/13d725f8c505642634282.jpg',
+    altText: 'Mohammad Abbass',
+    title: 'Mohammad Abbass',
+    designation: 'Full Stack Developer',
     socialProfile: [
       {
         id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
+        name: 'github',
+        path: 'https://github.com/Mohammad-y-abbass',
+        icon: <FaGithub />,
       },
       {
         id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
+        name: 'linkedin',
+        path: 'https://www.linkedin.com/in/mohammad-abbass',
+        icon: <FaLinkedin />,
       },
     ],
   },
   {
     id: 2,
-    imgSrc: Member2,
-    altText: 'Aaron Nunez',
-    title: 'Aaron Nunez',
-    designation: 'Founder',
+    imgSrc: 'https://telegra.ph/file/dda6c7c422f87d8c04f87.jpg',
+    altText: 'Phillip Venus',
+    title: 'Phillip Venus',
+    designation: 'Mobile Developer',
     socialProfile: [
       {
         id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
         name: 'twitter',
-        path: '#',
+        path: 'https://twitter.com/GPrincePhillip',
         icon: <FaTwitter />,
       },
       {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
+        id: 2,
+        name: 'github',
+        path: 'https://github.com/GrandPrince',
+        icon: <FaGithub />,
       },
     ],
   },
   {
     id: 3,
-    imgSrc: Member3,
-    altText: 'Aaron Nunez',
-    title: 'Aaron Nunez',
-    designation: 'Web Designer',
+    imgSrc: 'https://telegra.ph/file/48713eda71913759862c0.jpg',
+    altText: 'Borna Krpan',
+    title: 'Borna Krpan',
+    designation: 'Software Developer',
     socialProfile: [
       {
         id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
+        name: 'discord',
+        path: 'https://discord.com/users/429328860648308766',
+        icon: <FaDiscord />,
       },
       {
         id: 2,
         name: 'twitter',
-        path: '#',
+        path: 'https://twitter.com/login',
         icon: <FaTwitter />,
       },
       {
         id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
+        name: 'facebook',
+        path: 'https://www.facebook.com/borna.krpan77',
+        icon: <FaFacebookF />,
+      },
+      {
+        id: 4,
+        name: 'linkedin',
+        path: 'https://www.linkedin.com/in/borna-krpan',
+        icon: <FaLinkedin />,
       },
     ],
   },
   {
     id: 4,
-    imgSrc: Member4,
-    altText: 'Lina Jutila',
-    title: 'Lina Jutila',
-    designation: 'Web Developer',
+    imgSrc: 'https://telegra.ph/file/006201e0df93a801291ed.jpg',
+    altText: 'Malik Mehtab',
+    title: 'Malik Mehtab',
+    designation: 'Bot Developer',
     socialProfile: [
       {
         id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
+        name: 'github',
+        path: 'https://github.com/themalik-g',
+        icon: <FaGithub />,
       },
       {
         id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
         name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 5,
-    imgSrc: Member5,
-    altText: 'Saimon Harmer',
-    title: 'Saimon Harmer',
-    designation: 'CEO and Founder',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 6,
-    imgSrc: Member6,
-    altText: 'Aaron Nunez',
-    title: 'Aaron Nunez',
-    designation: 'Web Designer',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
+        path: 'https://www.instagram.com/where_is_my_name_n_dp',
         icon: <FaInstagram />,
       },
     ],
@@ -181,26 +107,27 @@ export default function TeamSection() {
   return (
     <section>
       <Container>
-        <SectionHeader
-           slogan="our Team"
-           title= "The most qulified and talented induviduals"
-           />
-
+        <Heading as="h1" sx={{ textAlign: 'center', mb: 4 }}>Our Team</Heading>
         <Grid sx={styles.grid}>
-        {data.map((item) => (
-          <TeamCard 
-            key={item.id}
-            src={item.imgSrc}
-            altText={item.altText}
-            title={item.title}
-            designation={item.designation}
-            social={item.socialProfile}
-          />
-        ))}  
-          
-        </Grid>   
-
-           
+          {data.map((item) => (
+            <Box sx={styles.card} key={item.id}>
+              <Box sx={styles.imageContainer}>
+                <Image src={item.imgSrc} alt={item.altText} sx={styles.profileImage} />
+              </Box>
+              <Box sx={styles.info}>
+                <Heading as="h3">{item.title}</Heading>
+                <Text as="span">{item.designation}</Text>
+                <Box sx={styles.socialIcons}>
+                  {item.socialProfile.map((social) => (
+                    <Link key={social.id} href={social.path} sx={styles.socialIcon}>
+                      {social.icon}
+                    </Link>
+                  ))}
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Grid>
       </Container>
     </section>
   );
@@ -208,14 +135,45 @@ export default function TeamSection() {
 
 const styles = {
   grid: {
-    mt: [0, null, -6, null, -4],
-    gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
-    gridTemplateColumns: [
-      'repeat(2,1fr)',
-      null,
-      'repeat(2,1fr)',
-      null,
-      'repeat(3,1fr)',
-    ],
+    display: 'grid',
+    gridTemplateColumns: ['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)'],
+    gap: '30px',
+  },
+  card: {
+    textAlign: 'center',
+  },
+  imageContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    mb: 3,
+  },
+  profileImage: {
+    width: '120px',
+    height: '120px',
+    borderRadius: '50%',
+    border: '5px solid #f09433', // Instagram-like circle effect
+    padding: '3px',
+    background: 'linear-gradient(45deg, #f09433, #e6683c, #dc2743, #cc2366, #bc1888)',
+  },
+  info: {
+    h3: {
+      fontSize: '18px',
+      fontWeight: 'bold',
+      mt: 2,
+    },
+    span: {
+      fontSize: '14px',
+      color: '#666',
+    },
+  },
+  socialIcons: {
+    display: 'flex',
+    justifyContent: 'center',
+    mt: 3,
+    a: {
+      fontSize: '20px',
+      color: '#333',
+      mx: 2,
+    },
   },
 };
