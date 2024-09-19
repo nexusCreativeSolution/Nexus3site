98b0a06a9757f5064c41c2273bf9c8bd0fc254bc
NEXUS <publicbot34@gmail.com>
2024-09-18 21:17:24 -0500

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'startup product landing page',
+  description = 'nexus creative solution',
   author = 'JSM',
   meta,
-  title = 'startup landing title',
+  title = 'NexCreatives',
 }) {
   const metaData = [
     {
