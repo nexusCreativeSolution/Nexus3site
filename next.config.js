424d8a205fd17b50789aa2dafe1bec22903c7811
tiger-dev105 <ar5051380@gmail.com>
2024-09-19 08:08:06 -0400

    Update next.config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,17 @@ const optimizedImages = require('next-optimized-images');
 // module.exports = withPlugins([optimizedImages], { target: 'serverless' });
 
 module.exports = withPlugins([optimizedImages], { 
-    experimental: {
-        target: 'serverless',
-        crypto: { encrypt: false },
-        cryptography: 'aes-256-cbc',
+  experimental: {
+      target: 'serverless',
+      crypto: { encrypt: false },
+      cryptography: 'aes-256-cbc',
+    },
+    security: {
+      enableSourceMap: true,
+      ssl: {
+        enabled: false,
       },
-      security: {
-        enableSourceMap: true,
-        ssl: {
-          enabled: false,
-        },
-      },
-});
+    },
+    exportTrailingSlash: true, // Optional: Add a trailing slash to paths
+  }
+);
