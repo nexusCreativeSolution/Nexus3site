488c3c1c1cbfb09c7afd36d6c6568dbbb3ddf7dc
NEXUS <publicbot34@gmail.com>
2024-09-18 19:02:01 -0500

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -26,7 +26,7 @@ export default function Footer() {
             </nav>
           </Box>
           <Text sx={styles.footer.copyright}>
-            Copyright by {new Date().getFullYear()} Nikila Gimhan
+            Copyright by {new Date().getFullYear()} NexusCreativeSolution
           </Text>
         </Box>
       </Container>
